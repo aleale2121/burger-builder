25fa7228f53e5699846c0855e1a80e80f4bd1a35
aleale2121 <alefewyimer4@gmail.com>
2021-07-26 21:02:14 +0300

    remove old interceptors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import Layout from './hoc/Layouts/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-function App() {
-  return (
-    <div>
-      <Layout>
-       <BurgerBuilder/>
-      </Layout>
-    </div>
-  );
+import React, { Component } from "react";
+import Layout from "./hoc/Layouts/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
+class App extends Component {
+  // state = {
+  //   show: true,
+  // };
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false });
+  //   }, 5000);
+  // }
+  render() {
+    return (
+      <div>
+        <Layout>
+          {/* {this.state.show ? <BurgerBuilder/>:null} */}
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,16 +9,23 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
     componentWillMount() {
-      axios.interceptors.request.use((req) => {
+     this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
-      axios.interceptors.response.use(
-        (res) => res,
-        (error) => {
-          this.setState({ error: error });
-        }
-      );
+     this.resInterceptor = axios.interceptors.response.use(
+       (res) => res,
+       (error) => {
+         this.setState({ error: error });
+       }
+     );
+    }
+
+    componentWillUnmount() {
+      console.log('Will Unmount',this.reqInterceptor,this.resInterceptor);
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
+
     }
     errorConfirmHandler= () =>{
       this.setState({ error: null });
