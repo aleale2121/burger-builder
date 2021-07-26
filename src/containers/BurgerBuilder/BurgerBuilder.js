c97ff7ac708cd570da3f8337bdd1a307866debfa
aleale2121 <alefewyimer4@gmail.com>
2021-07-26 14:19:56 +0300

    post order to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,7 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
-
+import axios from "../../axios-orders";
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
@@ -91,6 +91,24 @@ class BurgerBuilder extends Component {
   };
   purchaseContinueHandler = () => {
     // alert("U continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalprice,
+      customer: {
+        name: "alefew ",
+        address: {
+          street: "street 1",
+          zipCode: "1000",
+          country: "Ethiopia",
+        },
+        email: "ale@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
   };
   render() {
     const disabledInfo = {
