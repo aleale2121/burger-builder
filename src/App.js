a63b35ed2484cf9825b78b4c8c923cbd18433881
aleale2121 <alefewyimer4@gmail.com>
2021-07-28 15:13:08 +0300

    implement navigation link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./hoc/Layouts/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import Checkout from "./containers/CheckOut/CheckOut";
+import Orders from "./containers/Orders/Orders";
 class App extends Component {
   // state = {
   //   show: true,
@@ -20,6 +21,7 @@ class App extends Component {
           {/* {this.state.show ? <BurgerBuilder/>:null} */}
           <Switch>
             <Route path="/checkout" component={Checkout} />
+            <Route path="/orders" component={Orders} />
             <Route path="/" component={BurgerBuilder} />
           </Switch>
         </Layout>
